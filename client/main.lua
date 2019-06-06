2b4fd8e92de5d3133c2fbeb26339cc70b497ffcb
XxFri3ndlyxX <18711345+XxFri3ndlyxX@users.noreply.github.com>
2019-06-06 16:44:34 -0400

    Fix marker not displaying after restarting scripts

--- client/main.lua
+++ client/main.lua
@@ -1,20 +1,6 @@
--- Local
-local Keys = {
-  ["ESC"] = 322, ["F1"] = 288, ["F2"] = 289, ["F3"] = 170, ["F5"] = 166, ["F6"] = 167, ["F7"] = 168, ["F8"] = 169, ["F9"] = 56, ["F10"] = 57,
-  ["~"] = 243, ["1"] = 157, ["2"] = 158, ["3"] = 160, ["4"] = 164, ["5"] = 165, ["6"] = 159, ["7"] = 161, ["8"] = 162, ["9"] = 163, ["-"] = 84, ["="] = 83, ["BACKSPACE"] = 177,
-  ["TAB"] = 37, ["Q"] = 44, ["W"] = 32, ["E"] = 38, ["R"] = 45, ["T"] = 245, ["Y"] = 246, ["U"] = 303, ["P"] = 199, ["["] = 39, ["]"] = 40, ["ENTER"] = 18,
-  ["CAPS"] = 137, ["A"] = 34, ["S"] = 8, ["D"] = 9, ["F"] = 23, ["G"] = 47, ["H"] = 74, ["K"] = 311, ["L"] = 182,
-  ["LEFTSHIFT"] = 21, ["Z"] = 20, ["X"] = 73, ["C"] = 26, ["V"] = 0, ["B"] = 29, ["N"] = 249, ["M"] = 244, [","] = 82, ["."] = 81,
-  ["LEFTCTRL"] = 36, ["LEFTALT"] = 19, ["SPACE"] = 22, ["RIGHTCTRL"] = 70,
-  ["HOME"] = 213, ["PAGEUP"] = 10, ["PAGEDOWN"] = 11, ["DELETE"] = 178,
-  ["LEFT"] = 174, ["RIGHT"] = 175, ["TOP"] = 27, ["DOWN"] = 173,
-  ["NENTER"] = 201, ["N4"] = 108, ["N5"] = 60, ["N6"] = 107, ["N+"] = 96, ["N-"] = 97, ["N7"] = 117, ["N8"] = 61, ["N9"] = 118
-}
-
 ESX = nil
 local HasAlreadyEnteredMarker   = false
 local LastZone                  = nil
-local PlayerData		            = {}
 local CurrentActionMsg          = ''
 local CurrentActionData         = {}
 local GUI                       = {}
@@ -31,28 +17,25 @@ Citizen.CreateThread(function()
 		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 		Citizen.Wait(0)
 	end
-	
+
 	while ESX.GetPlayerData().job == nil do
 		Citizen.Wait(10)
 	end
 
-	PlayerData = ESX.GetPlayerData()
+	ESX.PlayerData = ESX.GetPlayerData()
 end)
 
 RegisterNetEvent('esx:playerLoaded')
 AddEventHandler('esx:playerLoaded', function(xPlayer)
-  PlayerData = xPlayer
+	ESX.PlayerData = xPlayer
 end)
 
 RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob', function(xPlayer)
-  PlayerData.job = job
+AddEventHandler('esx:setJob', function(job)
+  ESX.PlayerData.job = job
 end)
 
---[[
-Function for drawing the impound lot blips on the map
-]]
-function drawImpoundLotMapBlips()
+Citizen.CreateThread(function()
   local zones = {}
   local blipInfo = {}
 
@@ -68,15 +51,6 @@ function drawImpoundLotMapBlips()
     EndTextCommandSetBlipName(blip)
     table.insert(DrawnMapBlips, blip)
   end
-end
-
---[[
-Thread for drawing the dropoff and retrieval markers for
-the impound lots
-]]
-Citizen.CreateThread(function()
-        drawImpoundLotMarkers()
-        drawImpoundLotMapBlips()
 end)
 
 --[[
@@ -138,11 +112,6 @@ AddEventHandler('esx_kuana_impound:hasEnteredMarker', function(zone)
     CurrentActionMsg  = "Press ~INPUT_LOOK_BEHIND~ to access the impound lot"
     CurrentActionData = {}
   end
-end)
-
-RegisterNetEvent('esx:setJob')
-AddEventHandler('esx:setJob', function(job)
-    drawImpoundLotMapBlips()
 end)
 
 AddEventHandler('esx_kuana_impound:hasExitedMarker', function(zone)
@@ -213,8 +182,9 @@ AddEventHandler("esx_kuana_impound:impound_nearest_vehicle", function(args)
     end
   end
 
-
-  function drawImpoundLotMarkers()
+Citizen.CreateThread(function()
+  while true do
+		Wait(0)
     local coords = GetEntityCoords(GetPlayerPed(-1))
 
     for k,v in pairs(Config.ImpoundLots) do
@@ -225,7 +195,7 @@ AddEventHandler("esx_kuana_impound:impound_nearest_vehicle", function(args)
       end
     end
   end
-
+end)
   --[[
   The primary impound menu
 
